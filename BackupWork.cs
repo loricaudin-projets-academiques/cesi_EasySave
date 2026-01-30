cc5d6459f0c46b65494d10b9435bea3ca2c43f70
SOUIBGUI-Amir <votre@email.com>
2026-01-30 10:22:19 +0100

    ccc

---
+++ BackupWork.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+
+namespace cesi_EasySave.Models
+{
+    public class BackupWork
+    {
+        public string Name { get; set; }
+        public string Name { get; set; }
+        public string DestinationPath { get; set; }
+
+
+
+    }
+}
