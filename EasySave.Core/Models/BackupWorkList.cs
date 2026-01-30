f60cf242f27a2579dbc9ed72246a61bd60a1af5d
SOUIBGUI-Amir <votre@email.com>
2026-01-30 14:29:47 +0100

    feature : Commit class BackupWorkList

---
+++ EasySave.Core/Models/BackupWorkList.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EasySave.Core.Models
+{
+    public class BackupWorkList
+
+    {
+        public List <BackupWork> liste { get; set; }
+
+        public BackupWorkList(List<BackupWork> liste)
+        {
+            this.liste = liste;
+           
+
+        }
+
+    }
+}
