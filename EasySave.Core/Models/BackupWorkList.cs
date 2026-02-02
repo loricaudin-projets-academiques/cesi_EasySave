4da1cf336831b80aac3e2a41d7bd72cca35b6927
loricaudin <146658637+loricaudin@users.noreply.github.com>
2026-02-02 09:48:23 +0100

    feature: Added remove BackupWork method (#5)
    * feature: Added remove BackupWork method
    
    * feature: Added Exception gestion for RemoveBackupWork

--- EasySave.Core/Models/BackupWorkList.cs
+++ EasySave.Core/Models/BackupWorkList.cs
@@ -14,9 +14,32 @@ namespace EasySave.Core.Models
         public BackupWorkList(List<BackupWork> liste)
         {
             this.liste = liste;
-           
-
         }
 
+        public bool RemoveBackupWork(BackupWork backupWork)
+        {
+            try
+            {
+                this.liste.Remove(backupWork);
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        public bool RemoveBackupWorkById(int id)
+        {
+            try
+            {
+                this.liste.RemoveAt(id);
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
     }
 }
