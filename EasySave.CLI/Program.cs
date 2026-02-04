8c2c91d147d767ddaf90b26ac0df1b921023f414
Loric Audin <loricaudin@yahoo.fr>
2026-02-04 23:49:12 +0100

    feat: Added Save in json file

--- EasySave.CLI/Program.cs
+++ EasySave.CLI/Program.cs
@@ -1,18 +1,35 @@
 ﻿using EasySave.Core.Models;
+using EasySave.Core.Services;
 
 namespace EasySave.CLI
 {
     internal class Program
     {
+        static JsonFileGestion jsonFileGestion = JsonFileGestion.GetInstance();
+        static BackupWorkList? backupWorkList;
+
         static void Main(string[] args)
         {
+            try
+            {
+                List<BackupWork>? backupWorks = jsonFileGestion.Open<List<BackupWork>>(BackupWorkList.JSON_FILE_PATH);
+                backupWorkList = new BackupWorkList(backupWorks);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                backupWorkList = new BackupWorkList();
+            }
+
             //Test
             BackupWork bw = new BackupWork(
+                    "Test",
                     @"C:\Users\audin\Téléchargements\Dossier 1",
                     @"D:\Dossier 2",
-                    "Test",
                     BackupType.DIFFERENTIAL_BACKUP
                 );
+
+            backupWorkList?.AddBackupWork(bw);
             bw.Execute();
         }
     }
