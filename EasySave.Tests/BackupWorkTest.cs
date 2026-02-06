e7ddd6c72ef25450e661b481f1b60c45095c6209
Loric Audin <loricaudin@yahoo.fr>
2026-02-06 12:21:13 +0100

    test: Added test for execute backupwork

--- EasySave.Tests/BackupWorkTest.cs
+++ EasySave.Tests/BackupWorkTest.cs
@@ -6,16 +6,66 @@ namespace EasySave.Tests
     public class BackupWorkTest
     {
         [Fact]
-        public void BackupWork_Create()
+        public void BackupWork_Execute()
         {
-            // Arrange : préparation des données
-            BackupWork backupWork = new BackupWork("Test", @"C:\Test_1", @"C:\Test_2", BackupType.DIFFERENTIAL_BACKUP);
+            string sourcePath = @"C:\Test_1";
+            string sourceDest = @"C:\Test_2";
 
-            // Act : exécution de l’action à tester
-            var result = backupWork.GetName() == "Test";
+            BackupWork backupWork_1 = new BackupWork("Test", sourcePath, sourceDest, BackupType.FULL_BACKUP);
+            BackupWork backupWork_2 = new BackupWork("Test", sourcePath, sourceDest, BackupType.DIFFERENTIAL_BACKUP);
 
-            // Assert : vérification du résultat
-            Assert.True(result);
+
+            try
+            {
+                Directory.CreateDirectory(sourcePath);
+                Directory.CreateDirectory(sourceDest);
+                File.WriteAllText(sourcePath + "\\test1.txt", "Bonjour");
+                backupWork_1.Execute();
+
+                bool result = Directory.GetFiles(sourcePath) != Directory.GetFiles(sourceDest);
+
+                Directory.Delete(sourcePath, true);
+                Directory.Delete(sourceDest, true);
+
+                Assert.True(result);
+            }
+            catch (Exception e)
+            {
+                Assert.Fail(e.Message);
+            }
+            try
+            {
+                Directory.CreateDirectory(sourcePath);
+                Directory.CreateDirectory(sourceDest);
+                backupWork_2.Execute();
+
+                bool result = Directory.GetFiles(sourcePath) != Directory.GetFiles(sourceDest);
+
+                Directory.Delete(sourcePath, true);
+                Directory.Delete(sourceDest, true);
+
+                Assert.True(result);
+            }
+            catch (Exception e)
+            {
+                Assert.Fail(e.Message);
+            }
+            try
+            {
+                backupWork_1.Execute();
+                Assert.Fail("Backup works must not succeed");
+            }
+            catch (Exception e)
+            {
+            }
+            try
+            {
+                backupWork_2.Execute();
+                Assert.Fail("Backup works must not succeed");
+            }
+            catch (Exception e)
+            {
+            }
         }
     }
 }
