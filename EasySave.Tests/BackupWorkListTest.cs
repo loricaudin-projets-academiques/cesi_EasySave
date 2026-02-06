c1597fa12e6bbddd4ec8a46de2001d8867a5dbf9
SOUIBGUI-Amir <votre@email.com>
2026-02-06 12:04:54 +0100

    Commit Add Update and Remove methods

---
+++ EasySave.Tests/BackupWorkListTest.cs
@@ -0,0 +1,63 @@
+﻿using EasySave.Core.Models;
+using Xunit;
+
+namespace EasySave.Tests
+{
+    public class BackupWorkListTest
+    {
+        [Fact]
+        
+        public void EditBackupWork_Test()
+        {
+            // Arrange
+            BackupWorkList list = new BackupWorkList();
+            BackupWork oldBackup = new BackupWork("OldName", @"C:\OldSource", @"C:\OldDest", BackupType.DIFFERENTIAL_BACKUP);
+            BackupWork newBackup = new BackupWork("NewName", @"C:\NewSource", @"C:\NewDest", BackupType.FULL_BACKUP);
+            list.AddBackupWork(oldBackup);
+
+            // Act
+            BackupWork? result = list.EditBackupWork(oldBackup, newBackup);
+
+            // Assert
+            Assert.NotNull(result);
+            Assert.Equal(1, list.GetCount());
+            Assert.Equal("NewName", result!.GetName());
+            Assert.Equal(@"C:\NewSource", result.GetSourcePath());
+            Assert.Equal(@"C:\NewDest", result.GetDestinationPath());
+            Assert.Equal(BackupType.FULL_BACKUP, result.GetBackupType());
+        }
+
+        [Fact]
+        public void RemoveBackupWork_Test()
+        {
+
+            BackupWorkList list = new BackupWorkList();
+            BackupWork backupWork = new BackupWork(@"C:\Test_1", @"C:\Test_2", "TestBackup", BackupType.DIFFERENTIAL_BACKUP);
+
+            list.AddBackupWork(backupWork);
+
+
+            bool result = list.RemoveBackupWork(backupWork);
+
+
+            Assert.True(result);
+            Assert.Equal(0, list.GetCount());
+        }
+
+        [Fact]
+
+        public void AddBackupWork_Test()
+        {
+            BackupWorkList list = new BackupWorkList();
+            for (int i = 0; i < 5; i++)
+            {
+                list.AddBackupWork(new BackupWork(@"C:\Source", @"C:\Destination", $"Backup{i}", BackupType.FULL_BACKUP));
+            }
+            Assert.Throws<Exception>(() =>
+            {
+                list.AddBackupWork(new BackupWork(@"C:\Source", @"C:\Destination", "Backup6", BackupType.FULL_BACKUP));
+            });
+        }
+
+    }
+}
