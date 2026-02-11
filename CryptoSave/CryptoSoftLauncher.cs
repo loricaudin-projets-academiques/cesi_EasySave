805e87d38bf1a9072d5d2b636b7d515ae1e9f1b6
SOUIBGUI-Amir <votre@email.com>
2026-02-11 15:44:37 +0100

    Implement CryptoSoft and modify daily logs done

---
+++ CryptoSave/CryptoSoftLauncher.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cesi_EasySave.CryptoSave
+{
+    class CryptoSoftLauncher
+    {
+        public int EncryptFile(string filePath)
+        {
+            ProcessStartInfo psi = new ProcessStartInfo
+            {
+                FileName = "CryptoSoft.exe",
+                Arguments = $"\"{filePath}\"",
+                UseShellExecute = false,
+                CreateNoWindow = true
+            };
+
+            using Process process = Process.Start(psi);
+            process.WaitForExit();
+
+            return process.ExitCode;
+        }
+    }
+}
