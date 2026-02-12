e387fde06ca564da4df1002bd817f334ef4324b2
amirsouibgui <135824719+amirsouibgui@users.noreply.github.com>
2026-02-12 08:48:55 +0100

    Implement CryptoSoft (#28)
    Co-authored-by: SOUIBGUI-Amir <votre@email.com>

---
+++ CryptoSave/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace CryptoSave
+{
+    class Program
+    {
+        static int Main(string[] args)
+        {
+          if (args.Length != 1)
+            {
+                Console.WriteLine("Usage: CryptoSoft.exe <filepath>");
+                return 1; 
+            }
+
+            
+            string fullPath = Path.GetFullPath(args[0]);
+
+            
+            if (!File.Exists(fullPath))
+            {
+                Console.WriteLine("Erreur : Fichier introuvable");
+                return 2; 
+            }
+
+           
+            try
+            {
+                CryptoService cryptoService = new CryptoService();
+                cryptoService.Encrypt(fullPath);
+                Console.WriteLine("Fichier crypté avec succès");
+                return 0; 
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Erreur lors du cryptage : {ex.Message}");
+                return -1; 
+            }
+        }
+    }
+}
