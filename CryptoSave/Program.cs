56e141363c45b01aa8afa693c81a37dcad84a3af
loricaudin <146658637+loricaudin@users.noreply.github.com>
2026-02-23 17:53:11 +0100

    feature: CryptoSoft blocked to mono-instance

--- CryptoSave/Program.cs
+++ CryptoSave/Program.cs
@@ -1,4 +1,6 @@
-﻿namespace CryptoSave
+﻿using System.Diagnostics;
+
+namespace CryptoSave
 {
     /// <summary>
     /// Entry point for CryptoSoft application.
@@ -13,6 +15,12 @@
         /// <returns>Exit code (0 = success, other = error).</returns>
         static int Main(string[] args)
         {
+            bool notepadRunning = Process.GetProcessesByName("CryptoSoft").Length > 1;
+            if (notepadRunning) {
+                Console.WriteLine("CryptoSoft est déjà en cours d'exécution.");
+                return 1;
+            }
+
             if (args.Length != 1)
             {
                 Console.WriteLine("Usage: CryptoSoft.exe <filepath>");
